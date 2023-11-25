2010e55756c7508403300d7f9f2bb149fdecdd62
msy20020410 <150320192+msy20020410@users.noreply.github.com>
2023-11-25 16:49:19 +0800

    Add files via upload

---
+++ My_Strcat/test.c
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h> 
+#include<assert.h>
+char* my_strcat(char* dest, const char* src)
+{
+	char* ret = dest;
+	assert(dest != NULL);
+	assert(src != NULL);
+	//ÕÒµ½Ð±¸Ü0
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+	//×·¼Ó
+	while (*dest++ = *src++)
+	{
+		;
+	}
+	return ret;
+}
+int main()
+{
+	char arr1[30] = "msy";
+	char arr2[] = " love pjj";
+	int sz = sizeof(arr2) / sizeof(arr2[0]);
+	my_strcat(arr1, arr2);
+	printf("%s\n", arr1);
+	return 0;
+}
